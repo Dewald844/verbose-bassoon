c6054db9d8f9da9ebe478891b56ed952c5a9ff08
Dewald Haasbroek <dewald844@gmail.com>
2025-06-05 15:28:04 +0200

    add first test

---
+++ TM.91.Domain.Tests/Domain.Tests.Helpers.cs
@@ -0,0 +1,18 @@
+﻿using TaskManager.Domain;
+using Xunit;
+public class EmailValidatorTests
+{
+    [Fact]
+    public void TestInvalidEmailString()
+    {
+        var invalidEmailString = "test.com";
+        Assert.False(EmailValidator.IsValidEmail(invalidEmailString));
+    }
+
+    [Fact]
+    public void TestValidEmailString()
+    {
+        var validEmailString = "test@test.co.za";
+        Assert.True(EmailValidator.IsValidEmail(validEmailString));
+    }
+}
