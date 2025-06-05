3207d79677bc01849e63d4f46409945df0800545
Dewald Haasbroek <dewald844@gmail.com>
2025-06-05 16:18:14 +0200

    Code cleanup

--- TM.91.Domain.Tests/Domain.User.Tests.cs
+++ TM.91.Domain.Tests/Domain.User.Tests.cs
@@ -4,13 +4,9 @@ using TaskManager.Domain;
 using TaskManager.Domain.Helpers;
 using Xunit;
 
-public class UserTests
+public class TestData
 {
-
-    [Fact]
-    public void TestUserNameUpdate()
-    {
-        var userTestData = new UserData
+    public static UserData UserTestData { get; set; } = new()
         {
             User_id = 1,
             User_email = "test@test.co.za",
@@ -19,21 +15,47 @@ public class UserTests
             Company_id = 1
         };
 
-        var userTestRole = UserRole.Admin;
+    public static UserRole UserAdminRole { get; set; } = UserRole.Admin;
 
-        var userTest = new User
+    public User UserAdminWithData { get; set; } = new()
         {
-            UserData = userTestData,
-            UserRole = userTestRole
+            UserData = UserTestData,
+            UserRole = UserAdminRole
         };
+}
 
-        var updateNameR_failure = userTest.UpdateUserName_R("");
-        var updateNameR_success = userTest.UpdateUserName_R("Test2");
+public class UserTests
+{
+
+    [Fact]
+    public void TestUserNameUpdate()
+    {
+        var testUser = new TestData();
+
+        var updateNameR_failure = testUser.UserAdminWithData.UpdateUserName_R("");
 
         Assert.True(updateNameR_failure.IsFailure);
         Assert.Equal(updateNameR_failure.Error, new Error("Error.InvalidNameString", ""));
-        
+
+        var updateNameR_success = testUser.UserAdminWithData.UpdateUserName_R("Test2");
+
         Assert.True(updateNameR_success.IsSuccess);
-        Assert.Equal("Test2", userTest.UserData.User_name);
+        Assert.Equal("Test2", testUser.UserAdminWithData.UserData.User_name);
+    }
+
+    [Fact]
+    public void TestUserEmailUpdate()
+    {
+        var testUser = new TestData();
+
+        var updateEmailR_failure = testUser.UserAdminWithData.UpdateUserEmail_R("");
+
+        Assert.True(updateEmailR_failure.IsFailure);
+        Assert.Equal(updateEmailR_failure.Error, new Error("Error.InvalidEmailString", ""));
+
+        var updateEmailR_success = testUser.UserAdminWithData.UpdateUserEmail_R("test2@test.co.za");
+
+        Assert.True(updateEmailR_success.IsSuccess);
+        Assert.Equal("test2@test.co.za", testUser.UserAdminWithData.UserData.User_email);
     }
 }
