9a8b6501a4bc3481a71e95b2393030ccc7f2fe5a
Dewald Haasbroek <dewald844@gmail.com>
2025-06-19 16:59:50 +0200

    udpate .gitignore

--- TM.91.Domain.Tests/Domain.User.Tests.cs
+++ TM.91.Domain.Tests/Domain.User.Tests.cs
@@ -32,12 +32,12 @@ public class UserTests
     {
         var testUser = new TestData();
 
-        var updateNameR_failure = testUser.UserAdminWithData.UpdateUserName_R("");
+        var updateNameR_failure = User.UpdateUserName_R("", testUser.UserAdminWithData);
 
         Assert.True(updateNameR_failure.IsFailure);
         Assert.Equal(updateNameR_failure.Error, new Error("Error.InvalidNameString", ""));
 
-        var updateNameR_success = testUser.UserAdminWithData.UpdateUserName_R("Test2");
+        var updateNameR_success = User.UpdateUserName_R("Test2", testUser.UserAdminWithData);
 
         Assert.True(updateNameR_success.IsSuccess);
         Assert.Equal("Test2", testUser.UserAdminWithData.UserData.UserName);
@@ -48,12 +48,12 @@ public class UserTests
     {
         var testUser = new TestData();
 
-        var updateEmailR_failure = testUser.UserAdminWithData.UpdateUserEmail_R("");
+        var updateEmailR_failure = User.UpdateUserEmail_R("", testUser.UserAdminWithData);
 
         Assert.True(updateEmailR_failure.IsFailure);
         Assert.Equal(updateEmailR_failure.Error, new Error("Error.InvalidEmailString", ""));
 
-        var updateEmailR_success = testUser.UserAdminWithData.UpdateUserEmail_R("test2@test.co.za");
+        var updateEmailR_success = User.UpdateUserEmail_R("test2@test.co.za", testUser.UserAdminWithData);
 
         Assert.True(updateEmailR_success.IsSuccess);
         Assert.Equal("test2@test.co.za", testUser.UserAdminWithData.UserData.UserEmail);
@@ -64,12 +64,12 @@ public class UserTests
     {
         var testUser = new TestData();
 
-        var updatePasswordR_failure = testUser.UserAdminWithData.UpdateUserPassword_R("", "NewSecureP@ss1");
+        var updatePasswordR_failure = User.UpdateUserPassword_R("", "NewSecureP@ss1", testUser.UserAdminWithData);
 
         Assert.True(updatePasswordR_failure.IsFailure);
         Assert.Equal(updatePasswordR_failure.Error, new Error("Error.InvalidCurrentPassword", ""));
 
-        var updatePasswordR_success = testUser.UserAdminWithData.UpdateUserPassword_R("Test1234", "NewSecureP@ss1");
+        var updatePasswordR_success = User.UpdateUserPassword_R("Test1234", "NewSecureP@ss1", testUser.UserAdminWithData);
 
         Assert.True(updatePasswordR_success.IsSuccess);
         Assert.Equal("NewSecureP@ss1", testUser.UserAdminWithData.UserData.UserPassword);
